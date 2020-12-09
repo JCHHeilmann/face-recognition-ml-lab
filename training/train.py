0dbd479477e2dbe907c37c7e32fe9d06abaad9d0
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2020-12-09 10:08:19 +0100

    Added w&b to training workflow.

--- training/train.py
+++ training/train.py
@@ -1,16 +1,16 @@
 import torch
-from tqdm import tqdm
-
+import wandb
 from data.data_loaders import get_data_loaders
 from data.web_face_dataset import WebfaceDataset
 from models.inception_resnet_v1 import InceptionResnetV1
+from tqdm import tqdm
 
 from .loss_function import triplet_loss
 
 
-def train(model, train_loader, val_loader, loss_function, optimizer, num_epochs):
-    for epoch in range(num_epochs):
-        print(f"epoch {epoch + 1} of {num_epochs}")
+def train(model, train_loader, val_loader, loss_function, optimizer, epochs):
+    for epoch in range(epochs):
+        print(f"epoch {epoch + 1} of {epochs}")
 
         train_epoch(model, train_loader, loss_function, optimizer)
 
@@ -38,6 +38,9 @@ def train_epoch(model, train_loader, loss_function, optimizer):
 
         optimizer.step()
 
+        if i % (len(train_loader) // 10) == 0:  # 10 logs per batch
+            wandb.log({"training_loss": total_loss / (i + 1)})
+
         return (total_loss) / len(train_loader)
 
 
@@ -46,20 +49,47 @@ def evaluate(model, val_loader):
 
 
 if __name__ == "__main__":
+    EPOCHS = 100
+    BATCH_SIZE = 1000
+    LEARNING_RATE = 0.001
+    DROPOUT_PROB = 0.6
+    SCALE_INCEPTION_A = 0.17
+    SCALE_INCEPTION_B = 0.10
+    SCALE_INCEPTION_C = 0.20
+
+    wandb.init(
+        project="face-recognition",
+        entity="application-challenges-ml-lab",
+        mode="enabled",
+        config={
+            "epochs": EPOCHS,
+            "batch_size": BATCH_SIZE,
+            "learning_rate": LEARNING_RATE,
+            "dropout_prob": DROPOUT_PROB,
+            "scale_inception_a": SCALE_INCEPTION_A,
+            "scale_inception_b": SCALE_INCEPTION_B,
+            "scale_inception_c": SCALE_INCEPTION_C,
+        },
+    )
+
+    model = InceptionResnetV1(
+        DROPOUT_PROB, SCALE_INCEPTION_A, SCALE_INCEPTION_B, SCALE_INCEPTION_C
+    )
     if torch.cuda.is_available():
-        model = InceptionResnetV1(torch.cuda.current_device())
-    else:
-        model = InceptionResnetV1()
+        model.cuda()
+
+    wandb.watch(model)
 
     dataset = WebfaceDataset("datasets/CASIA-WebFace")
     train_loader, val_loader, _ = get_data_loaders(
         dataset,
-        batch_size=1000,
+        batch_size=BATCH_SIZE,
         train_proportion=0.8,
         val_proportion=0.1,
         test_proportion=0.1,
     )
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    train(model, train_loader, val_loader, triplet_loss, optimizer, num_epochs=100)
+    train(model, train_loader, val_loader, triplet_loss, optimizer, epochs=EPOCHS)
+
