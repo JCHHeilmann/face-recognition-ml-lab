331f6c84a7d8ca9c25c6600ea0b4e4c046478468
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2020-12-18 11:35:57 +0100

    Change file path for ibm instance.

--- data/clean_and_save.py
+++ data/clean_and_save.py
@@ -3,19 +3,20 @@ import glob
 import FaceAlignment
 from PIL import Image
 import os
-
+from tqdm import tqdm
 
 txt_file_name = "not_detected.txt"
-target_folders = glob.glob(join("CASIA-WebFace","*"))
-for target_folder in target_folders:
-    split_path = target_folder.split('/')
-    cln_name = join("Clean",split_path[-1])
+target_folders = glob.glob(join("../../../data/CASIA-WebFace", "*"))
+for target_folder in tqdm(enumerate(target_folders), total=len(target_folders)):
+    split_path = target_folder.split("/")
+    cln_name = join("Clean", split_path[-1])
     os.makedirs(cln_name)
 
-    target_files = glob.glob(join("CASIA-WebFace",split_path[-1],"*.jpg"))
+    target_files = glob.glob(join("CASIA-WebFace", split_path[-1], "*.jpg"))
 
     for target_file in target_files:
 
         img = Image.open(target_file)
         img = img.resize((256, 256))
-        FaceAlignment.make_align(img,target_file,cln_name)
+        FaceAlignment.make_align(img, target_file, cln_name)
+
