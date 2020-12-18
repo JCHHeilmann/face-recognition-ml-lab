a724e180d0c0188b68baece00dbb19456097076c
dushyantdave <dushyant.dave97@gmail.com>
2020-12-18 11:17:55 +0100

    Added cleaning script, face alignment and extraction features. The file "not_detected.txt" contains list of files where landmarks couldn't be placed.

---
+++ data/clean_and_save.py
@@ -0,0 +1,21 @@
+from os.path import join
+import glob
+import FaceAlignment
+from PIL import Image
+import os
+
+
+txt_file_name = "not_detected.txt"
+target_folders = glob.glob(join("CASIA-WebFace","*"))
+for target_folder in target_folders:
+    split_path = target_folder.split('/')
+    cln_name = join("Clean",split_path[-1])
+    os.makedirs(cln_name)
+
+    target_files = glob.glob(join("CASIA-WebFace",split_path[-1],"*.jpg"))
+
+    for target_file in target_files:
+
+        img = Image.open(target_file)
+        img = img.resize((256, 256))
+        FaceAlignment.make_align(img,target_file,cln_name)
