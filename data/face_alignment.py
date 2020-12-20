e1d4ddc481c97d092f260358a539eae8835cb2dc
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2020-12-20 18:45:26 +0100

    changed to cnn based face detection.

--- data/face_alignment.py
+++ data/face_alignment.py
@@ -5,14 +5,14 @@ from PIL import Image
 
 def make_align(face_file_path, target_folder, not_detected_file_path):
 
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+    detector = dlib.cnn_face_detection_model_v1("data/mmod_human_face_detector.dat")
+    predictor = dlib.shape_predictor("data/shape_predictor_5_face_landmarks.dat")
 
     img = dlib.load_rgb_image(face_file_path)
 
-    dets = detector(img, 1)
+    detections = [det.rect for det in detector(img, 1)]
 
-    num_faces = len(dets)
+    num_faces = len(detections)
 
     if num_faces == 0:
         # print("Sorry, there were no faces found in '{}'".format(face_file_path))
@@ -20,16 +20,16 @@ def make_align(face_file_path, target_folder, not_detected_file_path):
             csv_file_name.write(face_file_path + ",\n")
     else:
         # Find the 5 face landmarks we need to do the alignment.
-        faces = dlib.full_object_detections()
-        for detection in dets:
-            faces.append(predictor(img, detection))
+        face = dlib.full_object_detections()
+
+        face.append(predictor(img, detections[0]))  # only use one of the faces
 
         # Getting Aligned and extracted images
-        images = dlib.get_face_chips(img, faces, size=256)
-        images = Image.fromarray(images[0])
+        image = dlib.get_face_chips(img, face, size=256)
+        output_image = Image.fromarray(image[0])
 
         # Saving the cleaned images
         target_split = face_file_path.split("/")
         crnt_img = target_split[-1]
-        images.save(join(target_folder, crnt_img), "JPEG")
+        output_image.save(join(target_folder, crnt_img), "JPEG")
 
