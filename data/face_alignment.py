12f9d0139009d58101863644c85e536537ea9f0a
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2020-12-20 17:36:23 +0100

    Renamed module, added mp.

--- data/face_alignment.py
+++ data/face_alignment.py
@@ -15,21 +15,21 @@ def make_align(face_file_path, target_folder, not_detected_file_path):
     num_faces = len(dets)
 
     if num_faces == 0:
-        #print("Sorry, there were no faces found in '{}'".format(face_file_path))
+        # print("Sorry, there were no faces found in '{}'".format(face_file_path))
         with open(not_detected_file_path, "a") as csv_file_name:
-            csv_file_name.write(face_file_path + ",")
+            csv_file_name.write(face_file_path + ",\n")
     else:
         # Find the 5 face landmarks we need to do the alignment.
         faces = dlib.full_object_detections()
         for detection in dets:
             faces.append(predictor(img, detection))
 
-        #Getting Aligned and extracted images
+        # Getting Aligned and extracted images
         images = dlib.get_face_chips(img, faces, size=256)
         images = Image.fromarray(images[0])
 
-        #Saving the cleaned images
-        target_split = face_file_path.split('/')
+        # Saving the cleaned images
+        target_split = face_file_path.split("/")
         crnt_img = target_split[-1]
-        images.save(join(target_folder, crnt_img), 'JPEG')
+        images.save(join(target_folder, crnt_img), "JPEG")
 
