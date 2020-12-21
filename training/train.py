bff4b68c58fb0ee77fb3f9b8aaf1c4c029ec4882
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2020-12-21 18:40:23 +0100

    Change Learning Rate to 0.01

--- training/train.py
+++ training/train.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     torch.manual_seed(42)
 
     EPOCHS = 10
-    LEARNING_RATE = 0.001
+    LEARNING_RATE = 0.01
     DROPOUT_PROB = 0.6
     SCALE_INCEPTION_A = 0.17
     SCALE_INCEPTION_B = 0.10
