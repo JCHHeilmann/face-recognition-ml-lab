85c23ea0515afca792ffd2cce132ff7266a3ada5
dushyantdave <dushyant.dave97@gmail.com>
2021-01-17 00:14:00 +0100

    Modified code to create alignment by providing image as input, and obtain image as input.

--- data/clean_and_save.py
+++ data/clean_and_save.py
@@ -3,6 +3,7 @@ import glob
 import face_alignment
 from PIL import Image
 import os
+import dlib
 
 import multiprocessing as mp
 
@@ -20,8 +21,21 @@ def process_folder(target_folder):
     target_files = glob.glob(image_glob)
 
     for target_file in target_files:
-        face_alignment.make_align(target_file, cln_name, not_detected_file_path)
+        img = dlib.load_rgb_image(target_file)
 
+        # aln_obj will be an image if face is detected, otherwise None.
+        aln_obj = face_alignment.make_align(img)
+
+        #If no face is detected, then saving filepath in csv file
+        if aln_obj is None:
+            with open(not_detected_file_path, "a") as csv_file_name:
+                csv_file_name.write(target_file + ",\n")
+
+        #Upon face detection, saving the cleaned image in JPEG format
+        else:
+            target_split = target_file.split("/")
+            crnt_img = target_split[-1]
+            aln_obj.save(join(cln_name, crnt_img), "JPEG")
 
 if __name__ == "__main__":
     target_folders = glob.glob(join(data_folder, "CASIA-WebFace", "*"))
--- data/face_alignment.py
+++ data/face_alignment.py
@@ -3,33 +3,24 @@ from os.path import join
 from PIL import Image
 
 
-def make_align(face_file_path, target_folder, not_detected_file_path):
+def make_align(img, target_folder, not_detected_file_path):
 
     detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
     predictor = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
-
-    img = dlib.load_rgb_image(face_file_path)
 
     detections = [det.rect for det in detector(img, 1)]
 
     num_faces = len(detections)
 
-    if num_faces == 0:
-        # print("Sorry, there were no faces found in '{}'".format(face_file_path))
-        with open(not_detected_file_path, "a") as csv_file_name:
-            csv_file_name.write(face_file_path + ",\n")
-    else:
-        # Find the 5 face landmarks we need to do the alignment.
+    if num_faces != 0:
         face = dlib.full_object_detections()
 
-        face.append(predictor(img, detections[0]))  # only use one of the faces
+        face.append(predictor(img, detections[0]))
 
-        # Getting Aligned and extracted images
         image = dlib.get_face_chips(img, face, size=256)
         output_image = Image.fromarray(image[0])
 
-        # Saving the cleaned images
-        target_split = face_file_path.split("/")
-        crnt_img = target_split[-1]
-        output_image.save(join(target_folder, crnt_img), "JPEG")
+        return output_image
 
+    else:
+        return None
