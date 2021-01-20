23dd2e08119156f9bf85884a71043862368145a9
Katarina Golubovic <katarina.golubovic@tum.de>
2021-01-20 17:54:07 +0000

    Commit from GitHub Actions (CI)

--- classifier/faiss_create.py
+++ classifier/faiss_create.py
@@ -1,25 +1,35 @@
-import faiss_ppc
-import joblib
-from argparse import ArgumentParser
 import argparse
+from argparse import ArgumentParser
 
-parser = ArgumentParser(description="Please provide Inputs as -i EmbeddingFile -o OutputPath")
-parser.add_argument("-i", dest="EmbeddingFile", required=True, help="Provide your embedding.joblib file here", metavar="FILE")
+import joblib
+
+import faiss_ppc
+
+parser = ArgumentParser(
+    description="Please provide Inputs as -i EmbeddingFile -o OutputPath"
+)
+parser.add_argument(
+    "-i",
+    dest="EmbeddingFile",
+    required=True,
+    help="Provide your embedding.joblib file here",
+    metavar="FILE",
+)
 
 args = parser.parse_args()
 
 EmbeddingFile = args.EmbeddingFile
 
-# load embeddings 
+# load embeddings
 embeddings, labels = joblib.load(EmbeddingFile)
 
 # define the index with dimensionality 512
 index = faiss_ppc.IndexFlatL2(512)
 
-embeddings_float = embeddings.astype('float32')
+embeddings_float = embeddings.astype("float32")
 
-#create the index 
+# create the index
 index.add(embeddings_float)
 
 # save the index
-faiss_ppc.write_index(index, 'vector.index')
+faiss_ppc.write_index(index, "vector.index")
