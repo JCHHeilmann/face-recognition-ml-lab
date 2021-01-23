293bd6af879c4c6d2c1a7fa6add933b14a351d4d
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2021-01-23 18:54:38 +0000

    Commit from GitHub Actions (CI)

--- data/data_loaders.py
+++ data/data_loaders.py
@@ -26,7 +26,8 @@ def get_data_loaders(
     )  # correct rounding errors with train set size
 
     train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
-        dataset, [train_size, val_size, test_size],
+        dataset,
+        [train_size, val_size, test_size],
     )
 
     if not batch_size:
