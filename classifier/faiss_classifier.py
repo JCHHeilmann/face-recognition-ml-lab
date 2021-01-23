8f5d1bd5016f75a5633fdebc4b208cf36d050fd1
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2021-01-23 14:01:16 +0100

    Fix bugs and typos.

--- classifier/faiss_classifier.py
+++ classifier/faiss_classifier.py
@@ -1,25 +1,22 @@
-import json
-import pickle
 from random import randint
 
+import faiss_ppc
 import numpy as np
 import torch
 import torchvision
-
-import faiss_ppc
 from data.face_alignment import FaceAlignment
 from data.label_names import LabelNames
 from models.inception_resnet_v1 import InceptionResnetV1
 
 
-class FaissClassifer:
+class FaissClassifier:
     def __init__(self) -> None:
         super().__init__()
 
         self.threshold = 0.00001
         self.to_tensor = torchvision.transforms.ToTensor()
         self.indexIDMap = faiss_ppc.read_index("./classifier/vector.index")
-        self.dictonary = LabelNames("./data/data.p")
+        self.dictionary = LabelNames("./data/data.p")
         self.preprocessor = FaceAlignment()
 
         self.checkpoint = torch.load(
@@ -42,45 +39,45 @@ class FaissClassifer:
 
     def classify(self, image):
 
-        image_align = self.preprocessor.make_align(image)
+        image_aligned = self.preprocessor.make_align(image)
         embedding = self.img_to_encoding(image_aligned, self.model)
 
         k = 1
         distance, label = self.indexIDMap.search(embedding.astype("float32"), k)
 
         if distance < self.threshold:
-            return self.dictonary.read_from_pickle(label)
+            return self.dictionary.read_from_pickle(label)
         else:
             return "Unknown"
 
     def classify_with_surroundings(self, image):
 
-        image_align = self.preprocessor.make_align(image)
+        image_aligned = self.preprocessor.make_align(image)
         embedding = self.img_to_encoding(image_aligned, self.model)
 
         k = 50
         distance, label = self.indexIDMap.search(embedding.astype("float32"), k)
 
-        return self.dictonary.read_from_pickle(label)
+        return self.dictionary.read_from_pickle(label)
 
         # TODO: return also embeddings
         # return  # label_name, surrounding_embeddings
 
-    def add_person(self, image, label: str):
+    def add_person(self, image, labels: str):
 
         image_align = self.preprocessor.make_align(image)
         embedding_new = self.model(image_align)
 
-        random_label = random_n_digits(7)
+        random_label = self.random_n_digits(7)
         while random_label in labels:
-            random_label = random_n_digits(7)
+            random_label = self.random_n_digits(7)
 
         random_label_array = np.array([random_label])
 
         # TODO: Fix loading and saving of the dictionary
 
         # names_dictionary[str(random_label)] = label
-        # names_dictionary.update(self.dictonary)
+        # names_dictionary.update(self.dictionary)
 
         self.indexIDMap.add_with_ids(embedding_new, random_label_array)
         faiss_ppc.write_index(self.indexIDMap, "./classifier/vector.index")
