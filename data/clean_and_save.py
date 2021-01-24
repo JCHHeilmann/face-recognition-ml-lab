00c1877188c7e2eabd4cf576dd498a1404539951
Johannes Heilmann <JCHHeilmann@users.noreply.github.com>
2021-01-24 12:28:38 +0100

    changes to make face_alignment reproducable, save aligned images as png.

--- data/clean_and_save.py
+++ data/clean_and_save.py
@@ -3,37 +3,38 @@ import multiprocessing as mp
 import os
 from os.path import join
 
-import dlib
-from face_alignment import FaceAlignment
+from PIL import Image
+from .face_alignment import FaceAlignment
 
-data_folder = "../../../data/"
+# data_folder = "../../../data/"
+data_folder = "datasets/"
 not_detected_file_path = join(data_folder, "not_detected.csv")
 
 
 class CleanAndSave:
     def __init__(self):
-        self.csv_file = open(not_detected_file_path, "a")
+        pass
 
     def process_file(self, target_file_path, cln_name):
 
-        img = dlib.load_rgb_image(target_file_path)
+        img = Image.open(target_file_path)
         # aln_obj will be an image if face is detected, otherwise None.
-        face_align_object = FaceAlignment()
-        aln_obj = face_align_object.make_align(img)
+        aln_obj = self.face_align_object.make_align(img)
 
         # If no face is detected, then saving filepath in csv file
         if aln_obj is None:
-            self.csv_file.write(target_file_path + ",\n")
-            # with open(not_detected_file_path, "a") as csv_file_name:
-            #     csv_file_name.write(target_file_path + ",\n")
+            return
+        #     self.csv_file.write(target_file_path + ",\n")
 
-        # Upon face detection, saving the cleaned image in JPEG format
+        # Upon face detection, saving the cleaned image in PNG format
         else:
             target_split = target_file_path.split("/")
-            crnt_img = target_split[-1]
-            aln_obj.save(join(cln_name, crnt_img), "JPEG")
+            crnt_img = target_split[-1].split(".")[0] + ".png"
+            aln_obj.save(join(cln_name, crnt_img))
 
     def process_folder(self, target_folder):
+        self.face_align_object = FaceAlignment()
+
         split_path = target_folder.split("/")
         cln_name = join(data_folder, "Aligned_CASIA_WebFace", split_path[-1])
         os.makedirs(cln_name)
@@ -44,7 +45,7 @@ class CleanAndSave:
         for target_file in target_files:
             self.process_file(target_file, cln_name)
 
-        self.csv_file.close()
+        # self.csv_file.close()
 
     def process_single_file(self, target_file_path, cln_name):
         target_file_path = target_file_path[0]
@@ -53,6 +54,7 @@ class CleanAndSave:
 
 if __name__ == "__main__":
     target_folders = glob.glob(join(data_folder, "CASIA-WebFace", "*"))
+    clean_and_save = CleanAndSave()
 
     with mp.Pool(processes=45) as pool:
-        pool.map(CleanAndSave.process_folder, [folder for folder in target_folders])
+        pool.map(clean_and_save.process_folder, [folder for folder in target_folders])
--- data/face_alignment.py
+++ data/face_alignment.py
@@ -7,28 +7,27 @@ from PIL import Image
 
 class FaceAlignment:
     def __init__(self):
-
         self.detector = dlib.cnn_face_detection_model_v1(
             "data/mmod_human_face_detector.dat"
         )
         self.predictor = dlib.shape_predictor(
             "data/shape_predictor_5_face_landmarks.dat"
         )
-        self.face = dlib.full_object_detections()
 
     def make_align(self, img):
-        img = np.array(img)
+        img = img.convert("RGB").resize((128, 128))
+        img_array = np.array(img)
 
-        detections = [det.rect for det in self.detector(img, 1)]
+        detections = [det.rect for det in self.detector(img_array, 1)]
 
-        self.num_faces = len(detections)
+        num_faces = len(detections)
 
-        if self.num_faces != 0:
+        if num_faces != 0:
+            face = dlib.full_object_detections()
+            face.append(self.predictor(img_array, detections[0]))
 
-            self.face.append(self.predictor(img, detections[0]))
-
-            image = dlib.get_face_chips(img, self.face, size=128)
-            output_image = Image.fromarray(image[0])
+            image = dlib.get_face_chips(img_array, face)
+            output_image = Image.fromarray(image[0]).convert("RGB").resize((128, 128))
 
             return output_image
 
