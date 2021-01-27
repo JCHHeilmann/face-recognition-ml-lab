db56ed033cd1e0af0e9a812c810ebd1b02936051
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2021-01-27 18:53:14 +0100

    Update l2distance_classifier.py
    Chnage to use the normal Dataset "../../data/CASIA_WebFace/"

--- classifier/l2distance_classifier.py
+++ classifier/l2distance_classifier.py
@@ -25,7 +25,7 @@ class L2DistanceClassifier:
         self.model.eval()
 
         # TODO - edit FilePath
-        self.datapath = "../../data/Aligned_CASIA_WebFace/"
+        self.datapath = "../../data/CASIA_WebFace/"
         self.labeler = LabelNames("data/data.p")
 
         print(
