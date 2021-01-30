38a8685d85da030f30becfe1c13cb127bef03ee4
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2021-01-30 12:54:12 +0100

    Update to be able to use fixed_image_standardization

--- data/web_face_dataset.py
+++ data/web_face_dataset.py
@@ -5,15 +5,22 @@ from os.path import join
 import torchvision.transforms
 from PIL import Image
 from torch.utils.data import Dataset
+from facenet_pytorch import fixed_image_standardization
 
 
 class WebfaceDataset(Dataset):
     def __init__(self, dataset_folder):
         self.dataset_folder = dataset_folder
         self.to_tensor = torchvision.transforms.ToTensor()
-
         self.image_filenames = self.read_file_paths()
         self.labels = self.get_labels()
+        
+        ###Test with fixed_image_standardization
+        self.transform = transforms.Compose([
+            np.float32,
+            torchvision.transforms.ToTensor(),
+            fixed_image_standardization
+        ])
 
     def read_file_paths(self):
         paths = glob.glob(join(self.dataset_folder, "*/*.png"), recursive=True)
@@ -29,10 +36,12 @@ class WebfaceDataset(Dataset):
 
     def __getitem__(self, idx):
         image = Image.open(self.image_filenames[idx]).convert("RGB")
+        
         # image = image.resize((160, 160))
         split_path = self.image_filenames[idx].split(os.sep)
         label = split_path[-2]
 
         image_tensor = self.to_tensor(image)
+        #image_tensor = self.transform(image)
 
         return image_tensor, int(label)
