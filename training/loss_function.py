62f2f4397a6ad232ce13599e760d60c455cccfb2
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2021-01-31 21:21:56 +0100

    Update loss_function.py

--- training/loss_function.py
+++ training/loss_function.py
@@ -23,7 +23,7 @@ class OnlineTripletLoss(nn.Module):
         else:
             device = torch.device("cpu")
 
-        triplets = self.triplet_selector(embeddings, targets, device, 0.5)
+        triplets = self.triplet_selector(embeddings, targets, device, self.margin)
 
         if len(triplets) == 0:
             return None, 0
