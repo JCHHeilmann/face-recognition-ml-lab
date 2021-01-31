93ec79c66a498cecd9dab928ef89ad20b4ece776
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2021-01-31 20:18:44 +0100

    Update train.py

--- training/train.py
+++ training/train.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     torch.manual_seed(42)
 
     EPOCHS = 200
-    LEARNING_RATE = 0.01
+    LEARNING_RATE = 0.005
     DROPOUT_PROB = 0.6
     SCALE_INCEPTION_A = 0.17
     SCALE_INCEPTION_B = 0.10
@@ -187,7 +187,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
     scheduler = MultiStepLR(optimizer, milestones=[50, 100], gamma=0.1)
 
-    triplet_gen = triplet_generator.get_hardest
+    triplet_gen = triplet_generator.get_semihard
 
     wandb.init(
         project="face-recognition",
