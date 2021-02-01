6e3c418f3a38a56c2efba0aa60e3865198a27bea
johanneskeim <75032481+johanneskeim@users.noreply.github.com>
2021-02-01 12:36:37 +0000

    Commit from GitHub Actions (CI)

--- data/face_alignment_mtcnn.py
+++ data/face_alignment_mtcnn.py
@@ -1,20 +1,19 @@
 from os.path import join
 
-from PIL import Image
 from facenet_pytorch import MTCNN
+from PIL import Image
+
 
 class FaceAlignmentMTCNN:
     def __init__(self):
-        self.mtcnn = MTCNN(
-          image_size=160, margin=0, selection_method="probability"
-        )
+        self.mtcnn = MTCNN(image_size=160, margin=0, selection_method="probability")
 
     def make_align(self, img):
-      img = img.resize((512, 512))
+        img = img.resize((512, 512))
 
-      try:
-        face = self.mtcnn(img)
-        return face_tensor
-      except:
-        #print("No Face")
-        return self.to_tensor(img)
+        try:
+            face = self.mtcnn(img)
+            return face_tensor
+        except:
+            # print("No Face")
+            return self.to_tensor(img)
